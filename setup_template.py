5f99c5f894e9d033a6e1e1813d1aea9f803fa859
Johannes Bauer <joe@johannes-bauer.com>
2025-10-07 19:36:00 +0200

    Initial import
    First draft of listenwicht.

---
+++ setup_template.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name = "listenwicht",
+	packages = setuptools.find_packages(),
+	version = "${PACKAGE_VERSION}",
+	license = "gpl-3.0",
+	description = "Flexible Python-based mailing list daemon",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	author = "Johannes Bauer",
+	author_email = "joe@johannes-bauer.com",
+	url = "https://github.com/johndoe31415/listenwicht",
+	download_url = "https://github.com/johndoe31415/listenwicht/archive/v${PACKAGE_VERSION}.tar.gz",
+	keywords = [ "mailing", "list" ],
+	install_requires = [ "mailcoil>=0.0.7" ],
+	entry_points = {
+		"console_scripts": [
+			"listenwicht = listenwicht.__main__:main"
+		]
+	},
+	include_package_data = True,
+	classifiers = [
+		"Development Status :: 4 - Production/Beta",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3 :: Only",
+		"Programming Language :: Python :: 3.12",
+		"Programming Language :: Python :: 3.13",
+	],
+)
